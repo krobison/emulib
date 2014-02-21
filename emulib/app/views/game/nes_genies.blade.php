fe4e3cf6ed25ba47b1f07e35202cd7e44b808ccd
krobison <krobison@mines.edu>
2014-02-20 19:21:11 -0700

    JQuery work on gg table

--- emulib/app/views/game/nes_genies.blade.php
+++ emulib/app/views/game/nes_genies.blade.php
@@ -10,28 +10,57 @@
 	<div id="codetableholder">
 		<table class="table">
 			<tr class="tableheader success" id="show">
-				<td colspan="6">GameGenie (click to expand)</td>
+				<td colspan="6"><span id="genietext">GameGenie (click to expand)</span></td>
 			</tr>
 		</table>
-		<table class="table" id="codetable">
-			@foreach ($genies as $genie)
-				<tr class="showhide">
-					@if ($genie->twoColumn)
-						<td colspan="2" class="active" id="doublerow"> {{$genie->description}} </td>
-					@else
-						<td style="width:50%">{{$genie->code}} </td>
-						<td style="width:50%">{{$genie->description}} </td>
-					@endif
-				</tr>
-			@endforeach
-		</table>
+		<div id="codetablescrollholder">
+			<table class="table" id="codetable">
+				@foreach ($genies as $genie)
+					<tr class="showhide">
+						@if ($genie->twoColumn)
+							<td colspan="2" class="active" id="doublerow"> {{$genie->description}} </td>
+						@else
+							<td style="width:50%">{{$genie->code}} </td>
+							<td style="width:50%">{{$genie->description}} </td>
+						@endif
+					</tr>
+				@endforeach
+			</table>
+		</div>
 	</div>
 	
 @endif
 
 <script>
 	$("#show").click(function(){
-		$("#codetable").toggle(300);
+		$("#codetable").slideToggle(200);
+		
+		var genietext = $( "#genietext" ).text();
+		
+		if (genietext == "GameGenie (click to expand)"){
+			$('html, body').animate({
+				scrollTop: $("#codetable").offset().top-100
+			}, 200);
+			
+			$("#genietext").text("GameGenie (click to hide)");
+			
+			$("#codetablescrollholder").css({
+				height: '600px',
+				'overflow-y': 'auto',
+				'margin-bottom' : '64px'
+			});
+		}else{
+			$("#genietext").text("GameGenie (click to expand)");
+			
+			$("#codetablescrollholder").css({
+				height: 'auto',
+				'overflow-y': 'auto',
+				'margin-bottom' : '0px'
+			});
+		}
+		
 	});
-	$(document).ready(function() {$("#codetable").toggle();});
+	$(document).ready(function() {
+		$("#codetable").hide();
+	});
 </script>
