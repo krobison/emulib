d5f548f485d254bdfeb58c39efa95d04271141e4
krobison <krobison@mines.edu>
2014-02-20 19:41:09 -0700

    Default sprite

--- emulib/app/views/game/nes_reader.blade.php
+++ emulib/app/views/game/nes_reader.blade.php
@@ -7,7 +7,14 @@
 		<button type="button" class="btn btn-danger btn-sm" disabled="disabled" id="b_configure"><span class="glyphicon glyphicon-play"></span>&nbsp;&nbsp;Launch</button>
 	</div>
 	<div class="page-header">
-		<h3>NES Games <small>{{$game->name}}</small><img class="header_thumb" src={{asset('img/nes_sprite/'.$game->filename.'.png')}} /></h3>
+		<h3>NES Games 
+			<small>{{$game->name}}</small>
+			@if (file_exists(base_path().'/public/img/nes_sprite/'.$game->filename.'.png'))
+				<img class="header_thumb" src={{asset('img/nes_sprite/'.$game->filename.'.png')}} />
+			@else
+				<img class="header_thumb" src={{asset('img/nes_sprite/default.png')}} />
+			@endif
+		</h3>
 	</div>
 		<p class="bodytext">
 			{{$game->description}}
